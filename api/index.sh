54db8f0abc60c17c9783d3a806796099dc3746e3
Wojtek Bażant <wojtek.bazant@sanger.ac.uk>
2019-04-04 21:54:34 +0100

    x

---
+++ api/index.sh
@@ -0,0 +1,13 @@
+build() {
+  date > build-time.txt
+  curl ftp://ftp.ebi.ac.uk/pub/databases/wormbase/STAFF/wbazant/wbps11.schistosoma_mansoni_prjea36577.current_to_previous_id.tsv \
+    test.tsv
+}
+
+handler() {
+  echo "Build time:   $(cat build-time.txt)"
+  echo "Current time: $(date)"
+  echo ""
+  cat test.tsv
+}
+
